6d0a7fe531008bddf511402b4722c01328c2431b
Jose Lima <jalimag0206@gmail.com>
2023-12-03 15:54:09 -0300

    feat: add route to get meal by id

--- src/routes/meal.ts
+++ src/routes/meal.ts
@@ -4,6 +4,18 @@ import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 
 export async function mealRoutes(app: FastifyInstance) {
+    app.get('/:id', async (request) => {
+        const getMealSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = getMealSchema.parse(request.params)
+
+        const meal = await knex('meals').select('*').where('id', id).first()
+
+        return { meal }
+    })
+
     app.post('/', async (request, reply) => {
         const createMealSchema = z.object({
             name: z.string(),
