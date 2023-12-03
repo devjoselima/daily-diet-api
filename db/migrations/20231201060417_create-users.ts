4f66fbcda76f74894a6d98be194c19d3db02ba71
Jose Lima <jalimag0206@gmail.com>
2023-12-03 15:02:42 -0300

    chore: add new columns on table users

--- db/migrations/20231201060417_create-users.ts
+++ db/migrations/20231201060417_create-users.ts
@@ -6,6 +6,9 @@ export async function up(knex: Knex): Promise<void> {
         table.string('name').notNullable()
         table.string('email').notNullable()
         table.string('password').notNullable()
+        table.integer('total_meals').defaultTo(0)
+        table.integer('total_meal_inside_diet').defaultTo(0)
+        table.integer('total_meal_outsite_diet').defaultTo(0)
     })
 }
 
