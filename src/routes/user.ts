ca44ea789d0c99e5a68a1372525507d06ea5fa5c
Jose Lima <jalimag0206@gmail.com>
2023-12-03 19:18:28 -0300

    feat: ensure user can't be able to get user_id if is not existis

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,19 +4,28 @@ import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 
 export async function userRoutes(app: FastifyInstance) {
-    app.get('/:id', async (request) => {
-        const getUserSchema = z.object({
-            id: z.string().uuid(),
-        })
+    app.get('/:id', async (request, reply) => {
+        try {
+            const getUserSchema = z.object({
+                id: z.string().uuid(),
+            })
 
-        const { id } = getUserSchema.parse(request.params)
+            const { id } = getUserSchema.parse(request.params)
 
-        const user = await knex('users')
-            .select('*')
-            .where('user_id', id)
-            .first()
+            const user = await knex('users')
+                .select('*')
+                .where('user_id', id)
+                .first()
 
-        return { user }
+            if (!user) {
+                return reply.status(404).send({ error: 'Id is not found' })
+            }
+
+            return { user }
+        } catch (error) {
+            console.error(error)
+            return reply.status(500).send({ error: 'Internal Server Error' })
+        }
     })
 
     app.post('/', async (request, reply) => {
@@ -38,5 +47,17 @@ export async function userRoutes(app: FastifyInstance) {
             .returning('*')
 
         return reply.status(201).send(newUser)
+    })
+
+    app.delete('/:id', async (request, reply) => {
+        const deleteUserSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = deleteUserSchema.parse(request.params)
+
+        await knex('users').where('user_id', id).del()
+
+        return reply.status(200).send('Usuário deletado com sucesso!')
     })
 }
