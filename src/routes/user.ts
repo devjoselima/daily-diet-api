fad1467cabf0ff4fefae73ca9c7fa2c578ca717b
Jose Lima <jalimag0206@gmail.com>
2023-12-02 21:05:40 -0300

    feat: create route to get user by id

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,6 +4,21 @@ import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 
 export async function userRoutes(app: FastifyInstance) {
+    app.get('/:id', async (request) => {
+        const getUserSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = getUserSchema.parse(request.params)
+
+        const user = await knex('users')
+            .select('*')
+            .where('user_id', id)
+            .first()
+
+        return { user }
+    })
+
     app.post('/', async (request, reply) => {
         const createUserSchema = z.object({
             name: z.string(),
